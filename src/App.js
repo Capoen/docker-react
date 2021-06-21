e51fde2cdb166d25f8a98f823c2eef7da777ac9a
Arthur Capoen Rodrigues <capoen@MacBook-Pro-de-Arthur.local>
2021-06-21 15:53:09 -0300

    changed app text to enjoy react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch!
+          Enjoy react
         </a>
       </header>
     </div>
